90b771042beb3319f11f610859080089edad7b71
Rodrigo Borba <rodrigoborba@MacBook-Pro-de-Rodrigo.local>
2019-12-12 15:45:53 +0000

    merged

---
+++ starter-code/models/restaurants.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  id: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  latitude: {
+    type: String
+  },
+  longitude: {
+    type: String
+  },
+  averageCost: {
+    type: String
+  },
+  rating: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  comments: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Comments'
+  }
+});
+
+module.exports = mongoose.model('Restaurants', schema);
