6de4bd65edc74dcf740186208e2928535e1ac204
Rodrigo Borba <rodrigoborba@MacBook-Pro-de-Rodrigo.local>
2019-12-13 11:18:34 +0000

    restaurant list

--- starter-code/client/src/views/restaurants/RestaurantListView.jsx
+++ starter-code/client/src/views/restaurants/RestaurantListView.jsx
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  listRestaurants,
-  secondListRestaurants,
-  thirdListRestaurants,
-  fourthListRestaurants,
-  fifthListRestaurants
-} from "../../services/restaurantZomato";
+import { listRestaurants } from "../../services/restaurantZomato";
 import { Link } from "react-router-dom";
 
 class RestaurantListView extends Component {
@@ -100,3 +94,127 @@ class RestaurantListView extends Component {
   }
 }
 export default RestaurantListView;
+
+// import React, { Component, Fragment } from "react";
+// import { render } from "react-dom";
+// import { listRestaurants } from "../../services/restaurantZomato";
+// import request from "superagent";
+// import debounce from "lodash.debounce";
+
+// class InfiniteUsers extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     // Sets up our initial state
+//     this.state = {
+//       vegetarianRestaurants: null,
+//       error: false,
+//       hasMore: true,
+//       isLoading: false,
+//       users: []
+//     };
+
+//     // Binds our scroll event handler
+//     window.onscroll = debounce(() => {
+//       const {
+//         loadRestaurants,
+//         state: { error, isLoading, hasMore }
+//       } = this;
+
+//       // Bails early if:
+//       // * there's an error
+//       // * it's already loading
+//       // * there's nothing left to load
+//       if (error || isLoading || !hasMore) return;
+
+//       // Checks that the page has scrolled to the bottom
+//       if (
+//         window.innerHeight + document.documentElement.scrollTop ===
+//         document.documentElement.offsetHeight
+//       ) {
+//         loadRestaurants();
+//       }
+//     }, 100);
+//   }
+
+//   componentWillMount() {
+//     // Loads some users on initial load
+//     this.loadRestaurants();
+//   }
+
+//   loadRestaurants = () => {
+//     this.setState({ isLoading: true }, () => {
+//       request
+//         .get(listRestaurants)
+//         .then(results => {
+//           // Creates a massaged array of user data
+//           const nextRestaurants = results.body.results.map(user => ({
+//             email: user.email,
+//             name: Object.values(user.name).join(" "),
+//             photo: user.picture.medium,
+//             username: user.login.username,
+//             uuid: user.login.uuid
+//           }));
+
+//           // Merges the next users into our existing users
+//           this.setState({
+//             // Note: Depending on the API you're using, this value may
+//             // be returned as part of the payload to indicate that there
+//             // is no additional data to be loaded
+//             hasMore: this.state.users.length < 100,
+//             isLoading: false,
+//             users: [...this.state.users, ...nextRestaurants]
+//           });
+//         })
+//         .catch(err => {
+//           this.setState({
+//             error: err.message,
+//             isLoading: false
+//           });
+//         });
+//     });
+//   };
+
+//   render() {
+//     const { error, hasMore, isLoading, users } = this.state;
+
+//     return (
+//       <div>
+//         <h1>Infinite Users!</h1>
+//         <p>Scroll down to load more!!</p>
+//         {users.map(user => (
+//           <Fragment key={user.username}>
+//             <hr />
+//             <div style={{ display: "flex" }}>
+//               <img
+//                 alt={user.username}
+//                 src={user.photo}
+//                 style={{
+//                   borderRadius: "50%",
+//                   height: 72,
+//                   marginRight: 20,
+//                   width: 72
+//                 }}
+//               />
+//               <div>
+//                 <h2 style={{ marginTop: 0 }}>@{user.username}</h2>
+//                 <p>Name: {user.name}</p>
+//                 <p>Email: {user.email}</p>
+//               </div>
+//             </div>
+//           </Fragment>
+//         ))}
+//         <hr />
+//         {error && <div style={{ color: "#900" }}>{error}</div>}
+//         {isLoading && <div>Loading...</div>}
+//         {!hasMore && <div>You did it! You reached the end!</div>}
+//       </div>
+//     );
+//   }
+// }
+
+// const container = document.createElement("div");
+// document.body.appendChild(container);
+// render(<InfiniteUsers />, container);
+
+// export default InfiniteUsers;
