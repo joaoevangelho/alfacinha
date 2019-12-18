c13937d9f619818648e4d07399edfed9702666da
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-12-18 18:07:40 +0000

    starter-code deleted

---
+++ client/build/precache-manifest.8db7d93cc21394cc17f3f5447870510e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8caaf5bf9e6c9b1ecf28c3958ceb3a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "226a55c5fd7ca690aefb",
+    "url": "/static/css/2.d8899063.chunk.css"
+  },
+  {
+    "revision": "485d87e931150c43abef",
+    "url": "/static/css/main.d7621e75.chunk.css"
+  },
+  {
+    "revision": "226a55c5fd7ca690aefb",
+    "url": "/static/js/2.b4981500.chunk.js"
+  },
+  {
+    "revision": "17dc0d2b5a36ea9213a1a999ca79a735",
+    "url": "/static/js/2.b4981500.chunk.js.LICENSE"
+  },
+  {
+    "revision": "485d87e931150c43abef",
+    "url": "/static/js/main.e642d3a2.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
