6053584ce848fc9ab22383dd4ed657e548b0bb99
Joao Evangelho <joaoevangelho@MacBook-Pro-de-Joao-3.local>
2019-12-19 19:05:40 +0000

    shops info

--- client/src/views/Shops/ShopListView.jsx
+++ client/src/views/Shops/ShopListView.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Shop from '../../components/Shop';
 import { Link } from 'react-router-dom';
+import './style.css';
 
 import { listShops } from './../../services/shops';
 
@@ -25,23 +26,13 @@ class ListShopView extends Component {
     const shops = this.state.shops;
     console.log('whats this', shops);
 
-    return (
-      <div>
-        <h1>Shop List</h1>
-        <div className="product-list">
-          {shops.map(shop => {
-            return (
-              <div>
-                <Shop key={shop.name} {...shop} />
-                <Link to={`/shop-list/${shop.name}`}>
-                  <h5 className="card-title">{shop.name}</h5>
-                </Link>
-              </div>
-            );
-          })}
+    return shops.map(shop => {
+      return (
+        <div>
+          <Shop key={shop.name} {...shop} />
         </div>
-      </div>
-    );
+      );
+    });
   }
 }
 
