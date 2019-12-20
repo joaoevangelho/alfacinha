d41655ef8f32f54f603119e5782c6293c842b8e4
Joana Rozendo <joanarozendo@gmail.com>
2019-12-20 13:34:34 +0000

    styling

--- client/src/views/Authentication/JoinView.jsx
+++ client/src/views/Authentication/JoinView.jsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import Image from 'react-bootstrap/Image';
+import React, { Component } from "react";
+import Image from "react-bootstrap/Image";
 
-import { join as joinService } from '../../services/authentication';
+import { join as joinService } from "../../services/authentication";
 
-import './style.css';
+import "./style.css";
 
 class AuthenticationJoinView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      username: '',
-      aboutMe: '',
-      email: '',
-      password: '',
+      name: "",
+      username: "",
+      aboutMe: "",
+      email: "",
+      password: "",
       // image: null,
       error: false
     };
@@ -70,78 +70,79 @@ class AuthenticationJoinView extends Component {
 
     // const user = this.state.user;
     return (
-      <main className="pt-5 mt-5 mx-5 text-center d-flex justify-content-center">
-        <form
-          onSubmit={this.handleFormSubmission}
-          className="form-signin LoginJoinForm"
-        >
-          <Image
-            fluid
-            className="mb-4 LoginJoinImg"
-            src="/images/alfacinha-logo1.png"
-            alt=""
-          />
-          <label htmlFor="name" className="sr-only">
-            Name
-          </label>
-          <input
-            type="name"
-            placeholder="Name"
-            value={this.state.name}
-            name="name"
-            className="form-control mb-3"
-            onChange={this.handleInputChange}
-            required
-          />
-          <label htmlFor="email" className="sr-only">
-            Email
-          </label>
-          <input
-            type="email"
-            placeholder="Email"
-            value={this.state.email}
-            name="email"
-            className="form-control mb-3"
-            onChange={this.handleInputChange}
-            required
-          />
-          <label htmlFor="username" className="sr-only">
-            Username
-          </label>
-          <input
-            type="username"
-            placeholder="Username"
-            value={this.state.username}
-            name="username"
-            className="form-control mb-3"
-            onChange={this.handleInputChange}
-            required
-          />
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            name="password"
-            className="form-control mb-3"
-            onChange={this.handleInputChange}
-            required
-          />
-          <label htmlFor="name" className="sr-only">
-            About Me
-          </label>
-          <textarea
-            type="text"
-            placeholder="Write something about yourself here"
-            value={this.state.aboutMe}
-            name="aboutMe"
-            className="form-control mb-3"
-            onChange={this.handleInputChange}
-            required
-          ></textarea>
-          {/* <label htmlFor="image" className="sr-only">
+      <div className="BgColor pt-5">
+        <main className="pt-5 mx-5 text-center d-flex justify-content-center">
+          <form
+            onSubmit={this.handleFormSubmission}
+            className="form-signin LoginJoinForm"
+          >
+            <Image
+              fluid
+              className="mb-4 LoginJoinImg"
+              src="/images/alfacinha-logo1.png"
+              alt=""
+            />
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
+            <input
+              type="name"
+              placeholder="Name"
+              value={this.state.name}
+              name="name"
+              className="form-control mb-3"
+              onChange={this.handleInputChange}
+              required
+            />
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={this.state.email}
+              name="email"
+              className="form-control mb-3"
+              onChange={this.handleInputChange}
+              required
+            />
+            <label htmlFor="username" className="sr-only">
+              Username
+            </label>
+            <input
+              type="username"
+              placeholder="Username"
+              value={this.state.username}
+              name="username"
+              className="form-control mb-3"
+              onChange={this.handleInputChange}
+              required
+            />
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              name="password"
+              className="form-control mb-3"
+              onChange={this.handleInputChange}
+              required
+            />
+            <label htmlFor="name" className="sr-only">
+              About Me
+            </label>
+            <textarea
+              type="text"
+              placeholder="Write something about yourself here"
+              value={this.state.aboutMe}
+              name="aboutMe"
+              className="form-control mb-3"
+              onChange={this.handleInputChange}
+              required
+            ></textarea>
+            {/* <label htmlFor="image" className="sr-only">
             Image
           </label>
           <input
@@ -152,15 +153,16 @@ class AuthenticationJoinView extends Component {
             onChange={this.handleFileChange}
           /> */}
 
-          {this.state.error && (
-            <div class="alert alert-danger" role="alert">
-              Ops.. Email or username already exist!
-            </div>
-          )}
-          <button className="btn btn-lg MyBtn btn-block mb-5">Join</button>
-          {/* <p class="mt-5 mb-3 text-muted">&copy; 2019</p> */}
-        </form>
-      </main>
+            {this.state.error && (
+              <div class="alert alert-danger" role="alert">
+                Ops.. Email or username already exist!
+              </div>
+            )}
+            <button className="btn btn-lg MyBtn btn-block mb-5">Join</button>
+            {/* <p class="mt-5 mb-3 text-muted">&copy; 2019</p> */}
+          </form>
+        </main>
+      </div>
     );
   }
 }
--- client/src/views/Shops/SingleShopView.jsx
+++ client/src/views/Shops/SingleShopView.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
 //import Shop from '../../components/Shop';
-import { listShops } from './../../services/shops';
-import CommentList from './../Comments/CommentList';
-import MapBox from './../../components/MapBox';
+import { listShops } from "./../../services/shops";
+import CommentList from "./../Comments/CommentList";
+import MapBox from "./../../components/MapBox";
+import Button from "react-bootstrap/Button";
 
 class SingleShopView extends Component {
   constructor(props) {
@@ -41,59 +42,62 @@ class SingleShopView extends Component {
           <div>
             {shop.map(shop => {
               return (
-                // <ul key={shop.name}>
-                //   <h1>Single Product</h1>
-                //   <img src={shop.image} alt="img" />
-                //   <h1>{shop.name}</h1>
-                //   <h1>{shop.location}</h1>
-                //   <h1>{shop.contacts}</h1>
-                // </ul>
-                <div className="SingleRestaurantCard card m-5 p-5">
-                  <div className="row no-gutters">
-                    <div className="col-md-4">
-                      <img
-                        src={shop.image}
-                        className="card-img"
-                        alt="Shop Green"
-                      />
-                      <MapBox
-                        className="mt-5"
-                        lat="38.729287"
-                        lng="-9.144255"
-                      />
-                    </div>
-                    <div className="col-md-8">
+                <Fragment>
+                  <div className="SingleRestaurantCard card my-3 mx-4 p-0 no-gutters col-md-6 d-flex justify-content-center">
+                    {/* <div className="row no-gutters"> */}
+                    {/* <div className="col-md-6"> */}
+                    <div>
                       <div className="card-body">
                         <h5 className="card-title">{shop.name}</h5>
+                        <img
+                          src={shop.image}
+                          fluid
+                          className="card-image SingleRestImg m-0 p-0"
+                          alt="..."
+                        />
+
                         <p className="card-text">Address: {shop.location}</p>
                         <p className="card-text">Contact: {shop.contacts}</p>
+
                         {user && (
                           <div>
                             {(this.state.update && (
-                              <i
-                                // onClick={event => {
-                                //   this.removeFromFavoritesButton();
-                                // }}
-                                className="btn MyBtn"
+                              <Button
+                              // onClick={event => {
+                              //   this.removeFromFavoritesButton();
+                              // }}
+                              // className="btn MyBtn"
                               >
                                 Remove from Favorites
-                              </i>
+                              </Button>
                             )) || (
-                              <i
+                              <Button
                                 // onClick={event => {
-                                //   this.addToFavoritesButton(event, shop.name);
-                                // }}
+                                //   this.addToFavoritesButton(
+                                //     event,
+                                //     shop.name
+                                //   );
+
                                 className="btn MyBtn"
                               >
                                 Add to Favorites
-                              </i>
+                              </Button>
                             )}
                           </div>
                         )}
                       </div>
                     </div>
                   </div>
-                </div>
+                  {/* </div> */}
+                  <div className="col-md-6 mx-2 my-3">
+                    <MapBox
+                      className="SingleRestMap ml-5"
+                      lat="38.529287"
+                      lng="-9.144255"
+                    />
+                    {user && <CommentList {...this.props} />}
+                  </div>
+                </Fragment>
               );
             })}
           </div>
